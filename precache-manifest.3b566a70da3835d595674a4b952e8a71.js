bb2d160952bb585779081e29b3e16f85d4181f18
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-05-08 16:10:25 +0300

    Updates

---
+++ precache-manifest.3b566a70da3835d595674a4b952e8a71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5836caca2e342648dec6aef051ddf35c",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "8975c0a6ce9c6f73405b",
+    "url": "/tic-tac-toe/static/css/main.c1488c76.chunk.css"
+  },
+  {
+    "revision": "e79e9b6e42988ce451ed",
+    "url": "/tic-tac-toe/static/js/2.aecf43bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.aecf43bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8975c0a6ce9c6f73405b",
+    "url": "/tic-tac-toe/static/js/main.8e66d432.chunk.js"
+  },
+  {
+    "revision": "67093bcd19aa82d2fa67",
+    "url": "/tic-tac-toe/static/js/runtime-main.29e8e2be.js"
+  }
+]);
